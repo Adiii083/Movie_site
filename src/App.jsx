c3e5ee8291ec151b24a1cf45be9a5b1c7356cbf6
Adiii083 <adkr083@gmail.com>
2025-08-03 22:38:33 +0530

    Updated Movie Card

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {useState,useEffect} from 'react'
 import Search from './components/Search.jsx'
 import Spinner from './components/Spinner.jsx';
+import MovieCard from './components/MovieCard.jsx';
 
 const API_BASE_URL='https://api.themoviedb.org/3';
 const API_KEY=import.meta.env.VITE_TMDB_API_KEY;
@@ -64,7 +65,7 @@ const App=()=>{
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/> 
      </header>
      <section className='all-movie'>
-      <h2 className='mt-[40px]'>All Movies</h2>
+      <h2 className='mt-[60px]'>All Movies</h2>
 
       {
          isLoading ? (<Spinner/>):
@@ -73,7 +74,7 @@ const App=()=>{
             <ul>
               {
                  movieList.map((movie)=>(
-                  <p key={movie.id} className='text-white'>{movie.title}</p>
+                  <MovieCard key={movie.id} movie={movie} />
                  )
                 )
               }
